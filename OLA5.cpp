0a7f33111d0f197aef443674c13d044834f04cb6
joelrnorris <joel.r.norris@gmail.com>
2017-03-16 12:46:02 -0500

    initial commit, base project setup

---
+++ OLA5.cpp
@@ -0,0 +1,11 @@
+// OLA5.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include "parameters.h"
+
+int main()
+{
+    return 0;
+}
+
---
+++ parameters.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+
+//constant for testing with Fibonacci function
+
+const int FibVal = 10;
+
+//constant for testing with sum function
+
+const int SumVal = 13;
+
+//constant array for testing with Longest function 10 -4 3 7 8 0 10 2 22 14 -5 -2 7 0 1 2 3 4 5 6 7 8 9 10 11 12 value:
+
+const int LongestVal[13] = {10, -4, 3, 7, 8, 0, 10, 2, 22, 14, -5, -2, 7};
+
+//constant string for testing with shortest function
+
+const std::string SentenceVal = “C++ is a general purpose programming language with a bias towards systems programming that supports data abstraction”; 
