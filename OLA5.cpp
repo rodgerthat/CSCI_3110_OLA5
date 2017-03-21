0807d1ed8415616e362acca706d51d55f22cd0e2
joelrnorris <joel.r.norris@gmail.com>
2017-03-20 20:05:43 -0500

    added functions for fibonacci and sum

--- OLA5.cpp
+++ OLA5.cpp
@@ -1,11 +1,80 @@
-// OLA5.cpp : Defines the entry point for the console application.
-//
+/*
+ * Author: Norris, Joel R.
+ * Date: Mar 18, 2017
+ * File: ola5.cpp
+ * As a part of the 3110 assignment, this file
+ * contains the main program logic for the recursive functions
+ * as well as their function definitions and output
+ */
 
 #include <iostream>
 #include "parameters.h"
 
+using std::cout;
+using std::endl;
+
+
+// function definitions
+int fibonacci(int n);
+int sum(int n);
+int longest( *longestVal[]);
+
 int main()
 {
+
+	cout << endl << endl << "******************* OLA5 : RECURSION ********************" << endl << endl;
+
+	// call the fibonacci function, 
+	cout << "fibonacci sequence : " << endl;
+	for (int i = 0; i < FibVal; ++i)
+	{
+		cout << fibonacci(i) << " ";
+	}
+	cout << endl;
+
+	// call the sum function
+	cout << endl;
+	cout << "sum of all integers through " << SumVal << endl;
+	cout << sum(SumVal) << endl;
+
+	// call the longest value function
+	cout << endl;
+	cout << "the longest sequence of values in the array is : " << endl;
+	cout << longest(LongestVal) << endl;
+
+
     return 0;
+	
 }
 
+// fibonacci sequence function
+int fibonacci(int n) {
+
+	switch (n) {
+
+	case 0: return 0; break;
+	case 1: return 1; break;
+	default: return fibonacci(n - 1) + fibonacci(n - 2);
+
+	}
+
+}
+
+// sum sequence function
+// returns the sum of all the integers in a sequence through n
+int sum(int n) {
+
+	switch (n) {
+	case 0: return 0; break;
+	case 1: return 1; break;
+	default: return n + sum(n-1);
+	}
+}
+
+// longest sequence function
+// finds the longest positive sequence in an array
+int longest(int *longestVal[]) {
+	
+
+
+}
--- parameters.h
+++ parameters.h
@@ -1,3 +1,12 @@
+/*
+ * Author: Norris, Joel R.
+ * Date: Mar 18, 2017
+ * File: parameters.h
+ * As a part of the 3110 assignment, this file
+ * contains the parameters to pass to the functions
+ * in the main program 
+ */
+
 #pragma once
 #include <string>
 
@@ -9,10 +18,11 @@ const int FibVal = 10;
 
 const int SumVal = 13;
 
+// I don't know where some of those values came from, all I did was ctrl+c ctrl+v from MS Word OnLine
 //constant array for testing with Longest function 10 -4 3 7 8 0 10 2 22 14 -5 -2 7 0 1 2 3 4 5 6 7 8 9 10 11 12 value:
 
 const int LongestVal[13] = {10, -4, 3, 7, 8, 0, 10, 2, 22, 14, -5, -2, 7};
 
 //constant string for testing with shortest function
 
-const std::string SentenceVal = “C++ is a general purpose programming language with a bias towards systems programming that supports data abstraction”; 
+const std::string SentenceVal = "C++ is a general purpose programming language with a bias towards systems programming that supports data abstraction"; 
